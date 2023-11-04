cdbff3e459b1d122fce2f28fdf4c5ca42b03c02b
btopro <btopro@outlook.com>
2023-11-04 12:13:37 -0400

    remediations to enhance API

--- test/polaris-chip.test.js
+++ test/polaris-chip.test.js
@@ -1,12 +1,12 @@
 import { html } from 'lit';
 import { fixture, expect } from '@open-wc/testing';
 
-import '../src/polaris-chip.js';
+import '../src/polaris-tile-fact.js';
 
-describe('PolarisChip', () => {
+describe('PolarisTileFact', () => {
   let element;
   beforeEach(async () => {
-    element = await fixture(html`<polaris-chip></polaris-chip>`);
+    element = await fixture(html`<polaris-tile-fact></polaris-tile-fact>`);
   });
 
   it('renders a h1', () => {
